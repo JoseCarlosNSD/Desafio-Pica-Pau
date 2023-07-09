88af7192eeedd5f785f3fb93ae991e93c697a3b7
José Carlos Neto <zecarlosnetodantas@gmail.com>
2023-07-09 20:09:54 -0300

    att

---
+++ AlgoritmoPicaPau.js
@@ -0,0 +1,10 @@
+const picaPau = [];
+let lobo = 0;
+
+for(i = 1; i <= 10; i++){
+    lobo += 1;
+    picaPau.push(i);  
+    console.log(`Pica-Pau: ${lobo} pra você, ${picaPau} pra mim`);     
+}
+
+
